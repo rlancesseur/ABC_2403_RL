81b8b72a8e48c34f9226f418e47528b045eaf347
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-04-24 12:56:52 +0200

    Création fichier Java

---
+++ 03-Algorithmes/java/src/introduction/App.java
@@ -0,0 +1,29 @@
+package introduction;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		
+
+		System.out.println("Hello World !"); // Affiche Hello World dans la console
+		
+		
+		// VARIABLES
+		
+		String prenom; // Déclaration d'une variable "prenom" de type "chaine de caractères"
+		
+		int age; // Déclaration d'une variable "age" de type "nombre entier 32 Bits" (valeur scalaire)
+		         // (plage de valeurs d'un INT -2 147 000 000 à +2 147 000 000)
+		
+		long soldeBancaire; // Déclaration d'une variable "soldeBancaire" de type "nombre entier 64 Bits" (valeur scalaire)
+		
+		float tailleEnMetres; // Déclaration d'une variable "tailleEnMettres" de type "nombre réel 32 Bits" (valeur scalaire)
+		
+		double tailleEnMettresPourLesTresGrands; // Déclaration d'une variable "tailleEnMettresPourLesTresGrands" de type "nombre réel 64 Bits" (valeur scalaire)
+		
+		Boolean estMajeur; // Déclaration d'une variable "estMajeur" de type "Booléen" (valeur scalaire)
+		
+	}
+
+}
