8d48396dcb1299c14c91e169e22308674aac1824
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-04-25 12:52:39 +0200

    Exercice 1.5

--- 03-Algorithmes/calculSurfaceSecteurCirculaire/src/calculSurfaceSecteurCirculaire/App.java
+++ 03-Algorithmes/calculSurfaceSecteurCirculaire/src/calculSurfaceSecteurCirculaire/App.java
@@ -1,9 +1,35 @@
 package calculSurfaceSecteurCirculaire;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		float rayon;
+		
+		float angle;
+		
+		double aireSecteurCirculaire;
+		
+		Scanner sc;
+		
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Saisir le rayon du cercle");
+		rayon = sc.nextFloat();
+		
+		System.out.println("Saisir l'angle");
+		angle = sc.nextFloat();
+		
+		aireSecteurCirculaire = (Math.PI * Math.pow(rayon, 2) * angle) / 360;
+		
+		System.out.println("L'aire du secteur circulaire = " + aireSecteurCirculaire);
+		
+		sc.close();
+		
 
 	}
 
