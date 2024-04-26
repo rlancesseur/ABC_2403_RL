d26b4fe074934ca31a483f4377a355622c52b465
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-04-26 09:34:22 +0200

    Modification exercice 2.3

--- 03-Algorithmes/categorieFootball/src/categorieFootball/App.java
+++ 03-Algorithmes/categorieFootball/src/categorieFootball/App.java
@@ -1,10 +1,52 @@
 package categorieFootball;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int age;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir votre âge");
+		age = sc.nextInt();
+		
+		if(age <= 5) {
+			System.out.println("Vous êtes trop jeune");
+		}
+		else if(age > 6) {
+			System.out.println("Vous êtes débutant");
+		}
+		else if(age > 8) {
+			System.out.println("Vous êtes poussin");
+		}
+		else if(age > 10) {
+			System.out.println("Vous êtes benjamin");
+		}
+		else if(age > 12) {
+			System.out.println("Vous êtes pupille");
+		}
+		else if(age > 14) {
+			System.out.println("Vous êtes minime");
+		}
+		else if(age > 16) {
+			System.out.println("Vous êtes cadet");
+		}
+		else if(age > 18) {
+			System.out.println("Vous êtes junior");
+		}
+		else if(age > 34) {
+			System.out.println("Vous êtes sénior");
+		}
+		else if(age > 35) {
+			System.out.println("Vous êtes vétéran");
+		}
+
+		sc.close();
+		
 	}
 
 }
