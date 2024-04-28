9fd64e7bc39c2f54f7cc17972e4b97934cc11c6c
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-04-28 10:49:38 +0200

    Exercice 2.4 en Java

---
+++ 03-Algorithmes/predictionAvenir/src/predictionAvenir/App.java
@@ -0,0 +1,40 @@
+package predictionAvenir;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int heures;
+		int minutes;
+		int plus3Minutes;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir l'heure");
+		heures = sc.nextInt();
+		
+		System.out.println("Veuillez saisir les minutes");
+		minutes = sc.nextInt();
+		
+		
+		if (heures > 23) {
+			System.out.println("Les données saisie sont invalides");
+		}
+			else if (minutes > 68) {
+			System.out.println("Les données saisies sont invalides");
+			}
+				else {
+					
+				plus3Minutes = minutes + 3;
+			
+				System.out.println("Dans 3 minutes, il sera " + heures + " h " + plus3Minutes);
+				}
+		
+		sc.close();
+
+	}
+
+}
