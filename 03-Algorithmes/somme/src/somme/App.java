787cb563302a5378286638f5736532d7ecca04dd
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-05-21 09:03:46 +0200

    Exercice 3.4

--- 03-Algorithmes/somme/src/somme/App.java
+++ 03-Algorithmes/somme/src/somme/App.java
@@ -7,17 +7,15 @@ public class App {
 	public static void main(String[] args) {
 		
 		int nombreUtilisateur;
-		int i = 1;
 		int somme = 0;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Saisir un nombre");
 		nombreUtilisateur = sc.nextInt();
 		
-		while (i <= nombreUtilisateur)
+		for (int i = 0; i <= nombreUtilisateur; i++)
 		{
 			somme += i;
-			i++;
 		}
 		
 		System.out.println("La somme des nombres jusqu'à " + nombreUtilisateur + " est : " + somme);
