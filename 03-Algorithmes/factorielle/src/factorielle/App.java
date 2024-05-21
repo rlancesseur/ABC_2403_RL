333ae0d1336508f1319f17750d7bc113d6e986dd
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-05-21 09:04:10 +0200

    Exercice 3.5

--- 03-Algorithmes/factorielle/src/factorielle/App.java
+++ 03-Algorithmes/factorielle/src/factorielle/App.java
@@ -7,17 +7,15 @@ public class App {
 	public static void main(String[] args) {
 		
 		int nombreUtilisateur;
-		int i = 1;
 		long factorielle = 1;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Saisir un nombre : ");
 		nombreUtilisateur = sc.nextInt();
 		
-		while (i <= nombreUtilisateur)
+		for (int i = 1; i <= nombreUtilisateur; i++)
 		{
 			factorielle *= i;
-			i++;
 		}
 		
 		System.out.println("La factorielle de " + nombreUtilisateur + " est " + factorielle);
