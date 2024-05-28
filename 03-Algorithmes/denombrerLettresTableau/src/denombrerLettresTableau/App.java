f57ef7689a5f91c642623c702608654a6b761c51
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-05-28 16:12:48 +0200

    Modification exercice 5.3

--- 03-Algorithmes/denombrerLettresTableau/src/denombrerLettresTableau/App.java
+++ 03-Algorithmes/denombrerLettresTableau/src/denombrerLettresTableau/App.java
@@ -7,7 +7,6 @@ public class App {
 	public static void main(String[] args) {
 		
 		String texte;
-		int compteur = 0;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Saisissez un texte d'au moins 120 caractères : ");
@@ -19,17 +18,19 @@ public class App {
 		}
 		
 		String alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		String compteur[] = new String[1000];
 		
 		String tableau[] = texte.split("");
 		for (int i = 0; i < tableau.length; i++) 
 		{
-			if(alphabet[0] == tableau[i]) {
-				compteur += 1;
+			if(alphabet[i].contains(tableau[i])) {
+				compteur[i] += 1;
 			}
 		}
+		for (int i = 0; i < alphabet.length; i++) {
+		System.out.println("La lettre " + alphabet[i] + " apparait " + compteur[i] + " fois dans le texte.");
+		}
 		
-		System.out.println("La lettre " + alphabet[0] + " apparait " + compteur + " fois dans le texte.");
-
 		sc.close();
 		
 	}
