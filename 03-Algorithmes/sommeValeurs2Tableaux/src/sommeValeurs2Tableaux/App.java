8a250b775edf63b78b009df2df4deca30031446c
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-05-29 20:23:30 +0200

    Ajout fonction Exercice 4.5

--- 03-Algorithmes/sommeValeurs2Tableaux/src/sommeValeurs2Tableaux/App.java
+++ 03-Algorithmes/sommeValeurs2Tableaux/src/sommeValeurs2Tableaux/App.java
@@ -10,7 +10,7 @@ public class App {
 		
 		int tableau1[] = new int[5];
 		int tableau2[] = new int[5];
-		int tableauSomme[] = new int[tableau1.length];
+		int tableau3[] = new int[tableau1.length];
 		
 		System.out.println("Saisissez les 5 nombres du premier tableau : ");
 		for (int i = 0; i < 5; i++)
@@ -23,19 +23,30 @@ public class App {
 		{
 			tableau2[i] = sc.nextInt();
 		}
-		
-		for (int i = 0; i < tableau1.length; i++)
-		{
-			tableauSomme[i] = tableau1[i] + tableau2[i];
-		}
+	
+		App.somme(tableau1, tableau2, tableau3);
 		
 		System.out.print("La somme des valeurs des 2 tableaux précédents sont : " );
-		for (int i = 0; i < tableauSomme.length; i++)
+		for (int i = 0; i < tableau3.length; i++)
 		{
-		System.out.print(tableauSomme[i] + " " );
+		System.out.print(tableau3[i] + " " );
 		}
 		
 		sc.close();
 	}
 
+	/**
+	 * Fonction qui rentre la somme du tableau 1 et 2 dans le tableau 3
+	 * @param tableau1
+	 * @param tableau2
+	 * @param tableau3
+	 */
+	public static void somme(int tableau1[],int tableau2[],	int tableau3[]) {
+		
+		for (int i = 0; i < tableau1.length; i++)
+		{
+			tableau3[i] = tableau1[i] + tableau2[i];
+		}		
+	}
+	
 }
