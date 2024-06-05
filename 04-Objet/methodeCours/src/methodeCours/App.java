2b38bbb59eca9155f688f0065ae6b7c3eddb2b5e
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-06-05 16:48:24 +0200

    methodeCours

---
+++ 04-Objet/methodeCours/src/methodeCours/App.java
@@ -0,0 +1,49 @@
+package methodeCours;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		double resultat;
+		String prenom = "Mickaël";
+		String nom = "Devoldere";
+		String phrase;
+		
+		resultat = App.additionner(5, 10);
+		
+		System.out.println(resultat);
+		
+		phrase = App.concatenerPrenomNom(prenom, nom);
+		
+		System.out.println(phrase);
+		
+		App.afficherJourHeures();		
+		
+	}
+	
+	public static double additionner(double valeur1, double valeur2) {
+		double somme = 0;
+		somme = valeur1 + valeur2;
+		return somme;
+	}
+
+	public static void afficherJourHeures() {
+		
+		SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	    Date date = new Date();
+	    System.out.println("On est le " + s.format(date));
+		
+	}
+	
+	public static String concatenerPrenomNom(String prenom, String nom) {
+		
+		String retour;
+		retour = prenom + "." + nom;
+		return retour;
+		
+	}
+	
+}
