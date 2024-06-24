7065d4639adc6235d1ebdfd7b007cc3888851249
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-06-24 21:54:55 +0200

    Tests App

--- 04-Objet/bureauErgonomique/src/bureauErgonomique/App.java
+++ 04-Objet/bureauErgonomique/src/bureauErgonomique/App.java
@@ -10,13 +10,27 @@ public class App {
 		BureauElectriqueReglable bureauReglableTest = new BureauElectriqueReglable(100, 80, 200, 120, 80, true, 90, 100, 110);
 		
 		boolean aLaHauteur = bureauTest.positionnerALaHauteur(95);
-		boolean bureauMonte = bureauReglableTest.monterLeBureau(10);
-		bureauMonte = bureauWafae.descendreLeBureau(30);
+		aLaHauteur = bureauTest.positionnerALaHauteur(0);
+		aLaHauteur = bureauTest.positionnerALaHauteur(50);
+		
+		boolean bureauMonte = bureauWafae.monterLeBureau(10);
+		boolean bureauDescendu = bureauWafae.descendreLeBureau(30);
 		boolean priseBranche = bureauWafae.brancheLaPrise();
 		priseBranche = bureauWafae.debrancheLaPrise();
-		bureauMonte = bureauWafae.monterLeBureau(10);
 		priseBranche = bureauWafae.brancheLaPrise();
-		aLaHauteur = bureauWafae.positionnerALaHauteur(65);
+		bureauMonte = bureauWafae.monterLeBureau(10);
+		aLaHauteur = bureauWafae.positionnerALaHauteur(63);
+		aLaHauteur = bureauWafae.positionnerALaHauteur(64);
+		
+		aLaHauteur = bureauReglableTest.positionnerALaHauteur(130);
+		aLaHauteur = bureauReglableTest.positionnerALaHauteur(0);
+		aLaHauteur = bureauReglableTest.positionnerALaHauteur(115);
+		priseBranche = bureauReglableTest.brancheLaPrise();
+		bureauMonte = bureauReglableTest.monterLeBureau(10);
+		bureauDescendu = bureauReglableTest.descendreLeBureau(20);
+		aLaHauteur = bureauReglableTest.mettreALaHauteur1();
+		aLaHauteur = bureauReglableTest.mettreALaHauteur2();
+		aLaHauteur = bureauReglableTest.mettreALaHauteur3();
 
 	}
 
