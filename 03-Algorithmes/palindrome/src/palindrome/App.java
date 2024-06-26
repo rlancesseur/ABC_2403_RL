5badc2545cc4b48727707051d4f8222bf55584c3
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-06-26 16:23:20 +0200

    Add palindrome

---
+++ 03-Algorithmes/palindrome/src/palindrome/App.java
@@ -0,0 +1,28 @@
+package palindrome;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String saisieUtilisateur;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Saisissez une phrase : ");
+		saisieUtilisateur = sc.next();
+		
+		String tableau1[] = saisieUtilisateur.split("");
+		
+		String tableau2[] = new String[tableau1.length];
+		
+		for (int i = 0; i < saisieUtilisateur.length(); i++) {
+			
+			
+			
+		}
+		
+
+	}
+
+}
