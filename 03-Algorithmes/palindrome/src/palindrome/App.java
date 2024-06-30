8d03949639db249d478f441c514e54ca155d100e
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-06-30 19:38:30 +0200

    Exercice Palindrome

--- 03-Algorithmes/palindrome/src/palindrome/App.java
+++ 03-Algorithmes/palindrome/src/palindrome/App.java
@@ -9,19 +9,49 @@ public class App {
 		String saisieUtilisateur;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Saisissez une phrase : ");
-		saisieUtilisateur = sc.next();
+		do {
+		System.out.print("Saisissez une chaîne de caractères : ");
+		saisieUtilisateur = sc.nextLine().toLowerCase().replace(" ", "");
+		 
+			if(saisieUtilisateur.length() < 1) {
+			System.out.println("La phrase est vide.");
+			}
+		}
+		while (!saisieUtilisateur.contains(".") || saisieUtilisateur.contains(".") && saisieUtilisateur.length() < 2 );
 		
-		String tableau1[] = saisieUtilisateur.split("");
 		
-		String tableau2[] = new String[tableau1.length];
 		
-		for (int i = 0; i < saisieUtilisateur.length(); i++) {
-			
-			
-			
+		boolean palindrome = estUnPalindrome(saisieUtilisateur);
+		
+		
+		
+		if (palindrome) {
+			System.out.println("La chaîne de caractères est un palindrome.");
+		}
+		else {
+			System.out.println("La chaîne de caractères n'est pas un palindrome");
 		}
 		
+		sc.close();
+			
+	}		
+		
+		
+	public static boolean estUnPalindrome(String saisieUtilisateur) {
+			
+		String tableau[] = saisieUtilisateur.split("");
+		int i = 0;
+		int j = tableau.length - 2;
+		
+		while(i < j) {
+			
+			if(!tableau[i].contains(tableau[j])) {
+				return false;
+			}				
+			i ++;
+			j--;
+		}
+		return true;		
 
 	}
 
